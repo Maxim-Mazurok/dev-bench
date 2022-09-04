17fc4ff41054df3ef6c3c8ce642c19878ece1b70
Maxim Mazurok <maxim@mazurok.com>
2022-09-04 16:05:29 +1000

    update example

--- config.example.ts
+++ config.example.ts
@@ -1,8 +1,14 @@
-import { Project } from "./types";
+import { Engines } from "./nodeenv.js";
+import { Project } from "./types.js";
 
 // defaults
 export const runBenchmarksTimes = 3;
 export const coolDownBeforeAndBetweenBenchmarksSeconds = 5;
+export const defaultEngines: Engines = {
+  // will use this if there is no "engines" section in package.json
+  node: "16.13.0",
+  npm: "8.1.0",
+};
 
 // config
 export const projects: Project[] = [
@@ -16,6 +22,16 @@ export const projects: Project[] = [
         file: "tests/some.spec.js",
         search: "it('some test name',",
         replace: "xit('some test name',",
+      },
+      {
+        name: "delete some failing test",
+        file: "tests/docker.spec.js",
+        delete: true,
+      },
+      {
+        name: "ignore some folder",
+        file: ".gitignore",
+        append: "some-folder",
       },
     ],
     commands: [
